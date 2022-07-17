85526567e9ddcea7030bd3a1eeda9b27a8df6dd2
Javier <opennotice2015@gmai.com>
2022-07-16 23:07:15 -0400

    Shopping Aggregator

--- src/API.Gateways/Ocelot.APIGW/Program.cs
+++ src/API.Gateways/Ocelot.APIGW/Program.cs
@@ -1,9 +1,16 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
 
 var app = builder.Build();
-await app.UseOcelot();
+app.UseOcelot().Wait();
+
 app.Run();
